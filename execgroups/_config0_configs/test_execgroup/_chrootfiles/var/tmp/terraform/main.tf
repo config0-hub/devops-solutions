2a961bb8d30001397d3da0dff548ecaea76cbefc
gary <gary>
2024-07-05 22:39:36 +0800

    rendered and updated 07-05-2024-22-39

--- execgroups/_config0_configs/test_execgroup/_chrootfiles/var/tmp/terraform/main.tf
+++ execgroups/_config0_configs/test_execgroup/_chrootfiles/var/tmp/terraform/main.tf
@@ -207,3 +207,4 @@ output "name" { value = aws_vpc.main.tags.Name }
 output "route_table_private_id" { value = aws_route_table.private.id }
 output "route_table_public_id" { value = aws_default_route_table.public.id }
 
+
