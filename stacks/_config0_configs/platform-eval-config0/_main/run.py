87470056c8537f2a395e5611814019c82a3ba4e7
gary <gary>
2024-10-02 22:50:49 +0800

    rendered and updated 10-02-2024-22-50

--- stacks/_config0_configs/platform-eval-config0/_main/run.py
+++ stacks/_config0_configs/platform-eval-config0/_main/run.py
@@ -365,15 +365,15 @@ def run(stackargs):
                        ],
                        inputvars=["infracost"])
 
-    #stack.add_substack('config0-publish:::add_iac_ci',
-    #                   arguments=[
-    #                       cloud_tags_hash
-    #                   ],
-    #                   labels=[
-    #                       general,
-    #                       aws_cloud
-    #                   ],
-    #                   inputvars=["infracost"])
+    stack.add_substack('config0-publish:::add_iac_ci',
+                       arguments=[
+                           cloud_tags_hash
+                       ],
+                       labels=[
+                           general,
+                           aws_cloud
+                       ],
+                       inputvars=["infracost"])
 
     # ci with aws codebuild
     stack.add_substack('config0-publish:::setup_codebuild_ci',
