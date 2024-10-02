d244708009b6108c3aa2b6bbaad109fb803ae0f7
gary <gary>
2024-10-02 15:15:39 +0800

    rendered and updated 10-02-2024-15-15

--- stacks/_config0_configs/register_repo_iac_ci/_main/run.py
+++ stacks/_config0_configs/register_repo_iac_ci/_main/run.py
@@ -413,7 +413,7 @@ class Main(newSchedStack):
 
         sched = self.new_schedule()
         sched.job = "connect_repo"
-        sched.archive.timeout = 1200
+        sched.archive.timeout = 1800
         sched.archive.timewait = 120
         sched.automation_phase = "continuous_delivery"
         sched.human_description = "Connect repo with api gateway"
